37395a11ac8cb7ffbbbc282907320a3ff7b6cff6
Meilyn <[andrade.meiyn@gmail.com]>
2019-04-05 16:23:11 +0200

    First VueJS

---
+++ dist/precache-manifest.a2440bb9e93af6d9a03afdd7a4c24b52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e95a5a17e59a578e686",
+    "url": "/css/app.c1da38b4.css"
+  },
+  {
+    "revision": "0e95a5a17e59a578e686",
+    "url": "/js/app.65fb5124.js"
+  },
+  {
+    "revision": "97fe75698eb698c0cd59",
+    "url": "/js/chunk-vendors.2ec1d9f6.js"
+  },
+  {
+    "revision": "f60d30024c8acd2092a90a73bffe8af2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
